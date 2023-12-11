316f8497fbf7857939f1c56619d1fa31e7e5039a
Wesley-SF <wesleydefarias2@gmail.com>
2023-12-11 08:11:37 -0300

    Contador de partidas rankeadas

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
-console.log("Bom dia")
+function calcularNivel(vitorias, derrotas) {
+    const saldoRankeadas = vitorias - derrotas;
 
-let sabugo
-console.log("Bom dia")
+    let nivel;
+
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias >= 11 && vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias >= 21 && vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias >= 51 && vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias >= 81 && vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias >= 91 && vitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+
+    return { saldoRankeadas, nivel };
+}
+
+// Valores fixos para vitórias e derrotas
+const vitorias = 30;
+const derrotas = 15;
+
+const { saldoRankeadas, nivel } = calcularNivel(vitorias, derrotas);
+const mensagem = `O Herói tem um saldo de ${saldoRankeadas} e está no nível ${nivel}`;
+console.log(mensagem);
